78b5443da74001d81023a72402789b04cc91d8bf
anas77iqbl <164661344+anas77iqbl@users.noreply.github.com>
2024-03-26 23:08:16 +0500

    dice numb guess

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import inquirer from "inquirer";
 // 1) Computer will generate a random number
 // 2) User input for guessing number
 // 3) Compare user input with computer generated number and show result
-const randomNumber = Math.floor(Math.random() * 10 + 1);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number between 1-10",
+        message: "Please guess a number between 1-6",
     },
 ]);
 if (answer.userGuessedNumber === randomNumber) {
